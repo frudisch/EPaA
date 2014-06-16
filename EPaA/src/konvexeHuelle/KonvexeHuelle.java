43a63370cb63e8bfeb048ca88b65ad1ac5b21bf5
frudisch <FRudisch@gmx.de>
2014-06-16 09:56:25 +0200

    calculate left

--- EPaA/src/konvexeHuelle/KonvexeHuelle.java
+++ EPaA/src/konvexeHuelle/KonvexeHuelle.java
@@ -1,9 +1,64 @@
 package konvexeHuelle;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class KonvexeHuelle {
 
 	public KonvexeHuelle() {
-		// TODO Auto-generated constructor stub
+		ArrayList<Point> points = new ArrayList<Point>();
+		Point minimum;
+		
+		initArr(points);
+		minimum = findMin(points);
+		normalize(points, minimum);
+		print(points);
+		Collections.sort(points);
+		print(points);
+		calculate(points);
+		print(points);
+	}
+
+	private void calculate(ArrayList<Point> points) {
+		int temp;
+		
+		for(int i = 0; i < points.size(); i++){
+			for (int j = i; j < points.size(); j++) {
+				
+			}
+		}
+	}
+
+	private void normalize(ArrayList<Point> points, Point minimum) {
+		for (int i = 0; i < points.size(); i++) {
+			points.get(i).setX(points.get(i).getX() - minimum.getX());
+			points.get(i).setY(points.get(i).getY() - minimum.getY());
+		}
+	}
+
+	private Point findMin(ArrayList<Point> points ) {
+		Point temp;
+		temp = points.get(0);
+		for (int i = 1; i < points.size(); i++) {
+			if(temp.getY() > points.get(i).getY()){
+				temp = points.get(i);
+			}
+		}
+		return temp;
+	}
+	
+	private void print(ArrayList<Point> points) {
+		for (int i = 0; i < points.size(); i++) {
+			System.out.println(i + ". Punkt: " + points.get(i).getX() + " / " + points.get(i).getY());
+		}
+		
+	}
+	private void initArr(ArrayList<Point> points ) {
+		
+	}
+
+	public static void main(String[] args) {
+		KonvexeHuelle kh = new KonvexeHuelle();
 	}
 
 }
--- EPaA/src/konvexeHuelle/Point.java
+++ EPaA/src/konvexeHuelle/Point.java
@@ -1,9 +1,33 @@
 package konvexeHuelle;
 
-public class Point {
+public class Point implements Comparable<Point>{
 
-	public Point() {
-		// TODO Auto-generated constructor stub
+	private double x;
+	private double y;
+
+	public Point(double x, double y) {
+		this.setX(x);
+		this.setY(y);
+	}
+
+	public double getX() {
+		return x;
+	}
+
+	public void setX(double x) {
+		this.x = x;
+	}
+
+	public double getY() {
+		return y;
+	}
+
+	public void setY(double y) {
+		this.y = y;
+	}
+
+	public int compareTo(Point arg0) {
+		return (int) ((-1) * (arg0.getY() * x - arg0.getX() * y));
 	}
 
 }
