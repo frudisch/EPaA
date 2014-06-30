ec4f3cd71d98b35fd4119acde04efe41c2e52504
frudisch <FRudisch@gmx.de>
2014-06-30 11:36:58 +0200

    hull algorithmus verschnellert

--- EPaA/src/konvexeHuelle/HullAlg.java
+++ EPaA/src/konvexeHuelle/HullAlg.java
@@ -4,65 +4,67 @@ import java.util.Random;
 public class HullAlg {
 	
 	public static void main(String[] args) {
-		Point[] Pointe = new Point[15];
+		Point[] points = new Point[15];
 		
 		Random rand = new Random(150);
-		for (int i = 0; i < Pointe.length; i++) {
-			Pointe[i] = new Point(rand.nextDouble()*10, rand.nextDouble()*10);
+		for (int i = 0; i < points.length; i++) {
+			points[i] = new Point(rand.nextDouble()*10, rand.nextDouble()*10);
 		}
 		
 		HullAlg test = new HullAlg();
-		test.print(Pointe);
-		Pointe = test.huelle(Pointe);
-		test.print(Pointe);
+		test.print(points);
+		points = test.huelle(points);
+		test.print(points);
 	}
 	
 	
-	public Point[] huelle(Point[] Pointe){
-		int minInd = findMin(Pointe);
-		int maxInd = findMax(Pointe);
+	public Point[] huelle(Point[] points){
+		int minInd = findMin(points);
+		int maxInd = findMax(points);
 		
-		Point[] a = leftMax(minInd, maxInd, Pointe);
-		Point[] b = leftMax(maxInd, minInd, Pointe);
+		Point[] a = leftMax(minInd, maxInd, points);
+		Point[] b = leftMax(maxInd, minInd, points);
 		
-		Point[] temp = {Pointe[minInd], Pointe[maxInd]};
+		Point[] temp = {points[minInd], points[maxInd]};
 		
 		return concat(concat(a, b), temp);
 	}
-	private int findMax(Point[] Pointe) {
-		double max = Pointe[0].getX();
+	private int findMax(Point[] points) {
+		double max = points[0].getX();
 		int rc = 0;
 		
-		for (int i = 1; i < Pointe.length; i++) {
-			if(Pointe[i].getX() > max){
+		for (int i = 1; i < points.length; i++) {
+			if(points[i].getX() > max){
 				rc = i;
-				max = Pointe[i].getX();
+				max = points[i].getX();
 			}
 		}
 		
 		return rc;
 	}
-	private int findMin(Point[] Pointe) {
-		double min = Pointe[0].getX();
+	private int findMin(Point[] points) {
+		double min = points[0].getX();
 		int rc = 0;
 		
-		for (int i = 1; i < Pointe.length; i++) {
-			if(Pointe[i].getX() < min){
+		for (int i = 1; i < points.length; i++) {
+			if(points[i].getX() < min){
 				rc = i;
-				min = Pointe[i].getX();
+				min = points[i].getX();
 			}
 		}
 		
 		return rc;
 	}
-	private Point[] leftMax(int minInd, int maxInd, Point[] Pointe) {
+	private Point[] leftMax(int minInd, int maxInd, Point[] points) {
 		boolean found = false;
 		int tempInd = 0;
 		double tempFl = 0;
+		double tempFlLinks;
+		double tempFlRechts;
 		
-		for (int i = 0; i < Pointe.length; i++) {
-			if(flaeche(Pointe[minInd], Pointe[maxInd], Pointe[i]) > tempFl){
-				tempFl = flaeche(Pointe[minInd], Pointe[maxInd], Pointe[i]);
+		for (int i = 0; i < points.length; i++) {
+			if(flaeche(points[minInd], points[maxInd], points[i]) > tempFl){
+				tempFl = flaeche(points[minInd], points[maxInd], points[i]);
 				tempInd = i;
 				found = true;
 			}
@@ -70,16 +72,41 @@ public class HullAlg {
 		
 		if(found){
 			Point[] temp = new Point[1];
-			temp[0] = Pointe[tempInd];
+			temp[0] = points[tempInd];
 			
-			Point[] a = leftMax(minInd, tempInd, Pointe);
-			Point[] b = leftMax(tempInd, maxInd, Pointe);
+			for (int i = 0; i < points.length; i++) {
+				tempFl = flaeche(points[minInd], points[maxInd], points[i]);
+				tempFlLinks = flaeche(points[minInd], points[tempInd], points[i]);
+				tempFlRechts = flaeche(points[tempInd], points[maxInd], points[i]);
+				
+				if(tempFl > 0.0 && tempFlRechts < 0.0 && tempFlLinks < 0.0){
+					points = remove(points, i);
+					if(minInd > i)	--minInd;
+					if(maxInd > i) --maxInd;
+					if(tempInd > i) --tempInd;
+				}
+			}
+			
+			Point[] a = leftMax(minInd, tempInd, points);
+			Point[] b = leftMax(tempInd, maxInd, points);
 			return concat(concat(a,b), temp);
 		} else {
 			return new Point[0];
 		}
 		
 	}
+	
+	private Point[] remove(Point[] points, int i) {
+		Point[] rc = new Point[points.length - 1];
+		for (int j = 0; j < i; j++) {
+			rc[j] = points[j];
+		}
+		for (int j = i + 1; j < points.length; j++) {
+			rc[j - 1] = points[j];
+		}
+		return rc;
+	}
+
 	private Point[] concat(Point[] a, Point[] b) {
 		Point[] rc = new Point[a.length + b.length];
 		for (int i = 0; i < a.length; i++) {
