8f2dd260717872a56c48190246dbf4cc074fa86c
frudisch <FRudisch@gmx.de>
2014-07-07 22:51:17 +0200

    geraden algorithmus

--- EPaA/src/sweep_line/Schnittpunkte_Gerade.java
+++ EPaA/src/sweep_line/Schnittpunkte_Gerade.java
@@ -11,6 +11,7 @@ public class Schnittpunkte_Gerade {
 	
 	public void calculate (Line[] lines){
 		ArrayList<Line> activeLines = new ArrayList<Line>();
+		int j = 0;
 		
 		for (int i = 0; i < lines.length; i++) {
 			activeLines.add(lines[i]);
@@ -21,6 +22,20 @@ public class Schnittpunkte_Gerade {
 		for (int i = 0; i < lines.length; i++) {
 			lines[i] = activeLines.get(i);
 			activeLines.remove(i);
+		}
+		
+		for (int i = 0; i < lines.length; i++) {
+			j = i + 1;
+			
+			activeLines.add(lines[i]);
+			while(lines[i].getStart().getX() == lines[j].getStart().getX()){
+				activeLines.add(lines[j]);
+				++j;
+			}
+			
+			for (int k = 0; k < activeLines.size() - j - 1; k++) {
+				if(activeLines.get(k).getEnde().getX() == lines[i].getStart().getX()) activeLines.remove(k);
+			}
 		}
  	}
 }
